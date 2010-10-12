de2a316e95fc250a62b560f49089974dda93856f
Mohammed Morsi <mmorsi@redhat.com>
2010-10-11 21:59:25 -0400

    renamed 'appliance' to 'recipe', remove files not needed

--- deltacloud_appliance/manifests/file_with_dir.pp
+++
@@ -1,9 +0,0 @@
-# Wrapper around file, creating parent directory if it doesn't exist first
-define file_with_dir($dir = "", $source = "", $mode = ""){
-  file{"${dir}":
-       ensure => "directory" }
-  file{"${dir}/${name}":
-       source  => $source,
-       mode    => $mode,
-       require => File[$dir]}
-}
