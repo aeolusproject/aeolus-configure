680ac7e1b1df825a5a5c29716c73bf1707d99b39
Mike Orazi <morazi@redhat.com>
2011-01-10 17:25:43 -0500

    Populate name in /etc/yum.repos.d/*
    yumrepo populates repo id from name and name from descr.

--- recipes/deltacloud_recipe/manifests/deltacloud.pp
+++ recipes/deltacloud_recipe/manifests/deltacloud.pp
@@ -17,14 +17,17 @@ class deltacloud {
   # Setup repos which to pull deltacloud components
   yumrepo{"${name}_arch":
             name     => "${name}_arch",
+            descr    => "${name}_arch",
             baseurl  => 'http://repos.fedorapeople.org/repos/deltacloud/appliance/fedora-$releasever/$basearch',
             enabled  => 1, gpgcheck => 0}
   yumrepo{"${name}_noarch":
             name     => "${name}_noarch",
+            descr    => "${name}_noarch",
             baseurl  => 'http://repos.fedorapeople.org/repos/deltacloud/appliance/fedora-$releasever/noarch',
             enabled  => 1, gpgcheck => 0}
   yumrepo{"${name}_pulp":
             name     => "${name}_pulp",
+            descr    => "${name}_pulp",
             baseurl  => 'http://repos.fedorapeople.org/repos/pulp/pulp/fedora-13/$basearch/',
             enabled  => 1, gpgcheck => 0}
 }
