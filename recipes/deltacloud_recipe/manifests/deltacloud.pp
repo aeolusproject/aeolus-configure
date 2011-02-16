523108a9ccc4fbea3d29d0e905e4408ca2df6ab7
Mohammed Morsi <mmorsi@redhat.com>
2011-02-16 14:28:00 -0500

    For RHEL, point to RHEL 6 repos
      slight reworking of morazi's original patch

--- recipes/deltacloud_recipe/manifests/deltacloud.pp
+++ recipes/deltacloud_recipe/manifests/deltacloud.pp
@@ -28,20 +28,29 @@ if $aeolus_enable_packages == "true" or $aeolus_enable_packages == "1" {
 
 # Base deltacloud class
 class deltacloud {
+  # TODO:  Don't hardcode these repos to RHEL-6
+  #  The issue is that $releasever resolves to something like 6Server
+  #  so we either need to have a repo per RHEL variant, or we need
+  #  to have smarter logic here
+  $base_url_release      = $operatingsystem ? { 'fedora' => "fedora-\$releasever",
+                                                'redhat' => 'rhel-6' }
+  $pulp_base_url_release = $operatingsystem ? { 'fedora' => "fedora-13",
+                                                 'redhat' => 'rhel5' }
+
   # Setup repos which to pull deltacloud components
   yumrepo{"${name}_arch":
             name     => "${name}_arch",
             descr    => "${name}_arch",
-            baseurl  => 'http://repos.fedorapeople.org/repos/aeolus/packages/fedora-$releasever/$basearch',
+            baseurl  => "http://repos.fedorapeople.org/repos/aeolus/packages/${base_url_release}/\$basearch",
             enabled  => 1, gpgcheck => 0}
   yumrepo{"${name}_noarch":
             name     => "${name}_noarch",
             descr    => "${name}_noarch",
-            baseurl  => 'http://repos.fedorapeople.org/repos/aeolus/packages/fedora-$releasever/noarch',
+            baseurl  => "http://repos.fedorapeople.org/repos/aeolus/packages/${base_url_release}/noarch",
             enabled  => 1, gpgcheck => 0}
   yumrepo{"${name}_pulp":
             name     => "${name}_pulp",
             descr    => "${name}_pulp",
-            baseurl  => 'http://repos.fedorapeople.org/repos/pulp/pulp/fedora-13/$basearch/',
+            baseurl  => "http://repos.fedorapeople.org/repos/pulp/pulp/${pulp_base_url_release}/\$basearch/",
             enabled  => 1, gpgcheck => 0}
 }
