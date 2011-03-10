7362f6dec03c3ae54147e47e48e4166c4498cd9a
Martyn Taylor <mtaylor@redhat.com>
2011-03-10 11:07:39 +0000

    Added Tests for Check Admin/Create Bucket

--- spec/verify_install_spec.rb
+++ spec/verify_install_spec.rb
@@ -33,4 +33,34 @@ describe "aeolus-configure install" do
       end
     end
   end
+
+  it "should correctly create an aeolus templates bucket" do
+    stdout = ""
+    Net::SSH.start(@hostname, @username, :password => @password) do |ssh|
+      ssh.exec!("/usr/bin/curl -X GET http://localhost:9090") do |channel, stream, data|
+        stdout << data if stream == :stdout
+      end
+    end
+    stdout.include?("http://localhost:9090/templates").should == true
+  end
+
+  it "should create a site admin for aeolus conductor" do
+    stdout = ""
+    Net::SSH.start(@hostname, @username, :password => @password) do |ssh|
+      ssh.exec!("echo 'RAILS_ENV=\"production\"' > /tmp/check_admin.rb")
+      ssh.exec!("echo 'require \"/usr/share/aeolus-conductor/config/environment\"' >> /tmp/check_admin.rb")
+      ssh.exec!("echo 'user=User.find(:all, :conditions => {:login => \"admin\"}).first' >> /tmp/check_admin.rb")
+      ssh.exec!("echo 'puts \"email=\" + user.email' >> /tmp/check_admin.rb")
+      ssh.exec!("echo 'puts \"first_name=\" + user.first_name' >> /tmp/check_admin.rb")
+      ssh.exec!("echo 'puts \"last_name=\" + user.last_name' >> /tmp/check_admin.rb")
+      ssh.exec!("ruby /tmp/check_admin.rb") do |channel, stream, data|
+        stdout << data if stream == :stdout
+      end
+      stdout.include?("email=dcuser@aeolusproject.org").should == true
+      stdout.include?("first_name=aeolus").should == true
+      stdout.include?("last_name=user").should == true
+      ssh.exec!("rm -f /tmp/check_admin.rb")
+    end
+  end
+
 end
