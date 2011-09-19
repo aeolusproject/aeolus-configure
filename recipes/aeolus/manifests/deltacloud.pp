42f909def4fb6a5fd89095f6af46524c4a5be88d
Richard Su <rwsu@redhat.com>
2011-09-19 14:05:11 -0700

    Remove old deltacloud-<provider> init files
    Subject line correction.
    
    aeolus-cleanup does clean them up. But if you upgraded to a version of aeolus-configure
    that supported single deltacloud-core without running aeolus-cleanup, then these
    init files will remain in /etc/init.d.

--- recipes/aeolus/manifests/deltacloud.pp
+++ recipes/aeolus/manifests/deltacloud.pp
@@ -56,4 +56,11 @@ class aeolus::deltacloud::disabled {
       ensure  => 'stopped',
       enable  => false,
       hasstatus => true}
+
+    # remove deprecated services
+    file { '/etc/init.d/deltacloud-ec2-us-east-1': ensure => 'absent' }
+    file { '/etc/init.d/deltacloud-ec2-us-west-1': ensure => 'absent' }
+    file { '/etc/init.d/deltacloud-mock': ensure => 'absent' }
+    file { '/etc/init.d/deltacloud-rhevm': ensure => 'absent' }
+    file { '/etc/init.d/deltacloud-vsphere': ensure => 'absent' }
 }
