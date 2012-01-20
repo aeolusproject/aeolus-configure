82c37d94c32300cb30989e6b52e323cc098b2765
John Eckersberg <jeckersb@redhat.com>
2012-01-20 12:51:02 -0500

    Update rhevm and vsphere to support multiple configured providers

--- recipes/aeolus/manifests/profiles/rhevm.pp
+++ recipes/aeolus/manifests/profiles/rhevm.pp
@@ -12,42 +12,15 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-class aeolus::profiles::rhevm {
-  $missing = ensure_vardef("rhevm_nfs_server",
-                           "rhevm_nfs_export",
-                           "rhevm_nfs_mount_point",
-                           "rhevm_deltacloud_username",
-                           "rhevm_deltacloud_password",
-                           "rhevm_deltacloud_api",
-                           "rhevm_deltacloud_data_center",
-                           "rhevm_push_timeout")
-
-  if $missing {
-    fail("Missing required parameter ${missing} in /etc/aeolus-configure/nodes/rhevm_configure")
-  }
+class aeolus::profiles::rhevm ($instances) {
+  create_resources2('aeolus::profiles::rhevm::instance', $instances)
 
   file {"/etc/imagefactory/rhevm.json":
     content => template("aeolus/rhevm.json"),
-    mode => 755,
+    owner => root,
+    group => aeolus,
+    mode => 640,
     require => Package['aeolus-conductor-daemons'] }
-
-  aeolus::rhevm::validate{"RHEV NFS export validation":
-    rhevm_rest_api_url => "$rhevm_deltacloud_api",
-    rhevm_data_center => "$rhevm_deltacloud_data_center",
-    rhevm_username => "$rhevm_deltacloud_username",
-    rhevm_password => "$rhevm_deltacloud_password",
-    rhevm_nfs_export => "$rhevm_nfs_export"
-  }
-
-  file {"$rhevm_nfs_mount_point":
-    ensure => 'directory'}
-
-  mount {"$rhevm_nfs_mount_point":
-    ensure => mounted,
-    device => "$rhevm_nfs_server:$rhevm_nfs_export",
-    fstype => "nfs",
-    options => "rw",
-    require => [File["$rhevm_nfs_mount_point"], Aeolus::Rhevm::Validate['RHEV NFS export validation']]}
 
   aeolus::create_bucket{"aeolus":}
 
@@ -60,12 +33,6 @@ class aeolus::profiles::rhevm {
   aeolus::conductor::login{"admin": password => "password",
      require  => Aeolus::Conductor::Site_admin['admin']}
 
-  aeolus::conductor::provider{"rhevm":
-    deltacloud_driver   => "rhevm",
-    url                 => "http://localhost:3002/api",
-    deltacloud_provider => "$rhevm_deltacloud_api;$rhevm_deltacloud_data_center",
-    require             => Aeolus::Conductor::Login["admin"] }
-
   aeolus::conductor::hwp{"hwp1":
       memory         => "512",
       cpu            => "1",
@@ -74,15 +41,15 @@ class aeolus::profiles::rhevm {
       require        => Aeolus::Conductor::Login["admin"] }
 
   aeolus::conductor::logout{"admin":
-    require    => [Aeolus::Conductor::Provider['rhevm'],
-                   Aeolus::Conductor::Hwp['hwp1']] }
+    require    => Aeolus::Conductor::Hwp['hwp1']}
 
+  Aeolus::Conductor::Provider<| |> -> Aeolus::Conductor::Logout["admin"]
   # TODO: create a realm and mappings
 }
 
 define aeolus::rhevm::validate($rhevm_rest_api_url,$rhevm_data_center,$rhevm_username,$rhevm_password,$rhevm_nfs_export){
   $result = rhevm_validate_export_type($rhevm_rest_api_url,$rhevm_data_center,$rhevm_username,$rhevm_password,$rhevm_nfs_export)
-  notify {"RHEV NFS export validation":
+  notify {"${name}":
     message => "the RHEV NFS export is on the correct storage domain and has type 'export' => ${result}"
   }
 }
