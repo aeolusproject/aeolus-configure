e338373e61e64f461718d114251017beea1d4cdc
Steve Linabery <slinaber@redhat.com>
2012-06-27 12:27:31 -0500

    Revert "BZ835131 configure errors after upgrade"
    This reverts commit e304de30e98c98d8a8c3931fd71cf29a2602e6a5.
    
    Incorrect BZ number was referenced in commit message.

--- recipes/aeolus/manifests/conductor/site_admin.pp
+++ recipes/aeolus/manifests/conductor/site_admin.pp
@@ -5,13 +5,17 @@ define aeolus::conductor::site_admin($email="", $password="", $first_name="", $l
          environment => "RAILS_ENV=production",
          command     => "/usr/bin/rake dc:create_user[${name},${password},${email},${first_name},${last_name}]",
          logoutput   => true,
-         unless      => '/usr/bin/rake dc:admin_exists',
+         creates     => "/var/lib/aeolus-conductor/production.admin",
          require     => Aeolus::Rails::Seed::Db["seed_aeolus_database"]}
   exec{"grant_site_admin_privs":
          cwd         => '/usr/share/aeolus-conductor',
          environment => "RAILS_ENV=production",
          command     => "/usr/bin/rake dc:site_admin[${name}]",
          logoutput   => true,
-         subscribe   => Exec[create_site_admin_user],
-         refreshonly => true}
+         creates     => "/var/lib/aeolus-conductor/production.admin",
+         require     => Exec[create_site_admin_user]}
+  file{"/var/lib/aeolus-conductor/production.admin":
+         ensure => present,
+         recurse => true,
+         require => Exec['grant_site_admin_privs']}
 }
