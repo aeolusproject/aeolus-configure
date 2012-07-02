e93c068048cb233b7901ae96fc45021844f5c43e
mmorsi <mmorsi@redhat.com>
2012-07-02 13:38:07 -0400

    BZ #827587: do not modify ntp config
      still require package and service to be running
    
    Signed-off-by: Matt Wagner <matt.wagner@redhat.com>

--- recipes/ntp/manifests/client.pp
+++ recipes/ntp/manifests/client.pp
@@ -6,28 +6,4 @@ class ntp::client inherits ntp {
         hasstatus       => true,
         require         => Package["ntp"],
     }
-
-    # default ntp servers if none-specified
-    # for different environments this should be changed in the branch
-    # only setup an override here in case there is an odd host or two
-    # that needs to be different from others in the same env
-    $default_ntpservers = [ "pool.ntp.org" ]
-    $ntpservers = $ntpservers ? {
-        ''              => $default_ntpservers,
-        default         => $ntpservers,
-    }
-
-    file { "/etc/ntp.conf":
-        content         => template("ntp/ntp.conf"),
-        notify          => Service["ntpd"],
-        require         => Package["ntp"],
-    }
-    file { "/etc/ntp/":
-        require         => Package["ntp"],
-    }
-    file { "/etc/ntp/step-tickers":
-        content         => template("ntp/ntp.conf"),
-        notify          => Service["ntpd"],
-        require         => Package["ntp"],
-    }
 }
