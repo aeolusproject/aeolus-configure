f512cc6cba5870da8f431cfb298ec540985c4469
John Eckersberg <jeckersb@redhat.com>
2012-10-25 14:59:59 -0400

    run all rake tasks as the aeolus user (cherry picked from commit 742692131a97ce75bdba02870698e5bc949a3762)

--- recipes/aeolus/manifests/conductor/destroy_temp_admins.pp
+++ recipes/aeolus/manifests/conductor/destroy_temp_admins.pp
@@ -2,6 +2,7 @@ define aeolus::conductor::destroy_temp_admins {
   exec{"destroy_temp_admin-${name}":
     cwd         => '/usr/share/aeolus-conductor',
     environment => "RAILS_ENV=production",
+    user        => 'aeolus',
     command     => "rake dc:destroy_users_by_pattern[temporary-administrative-user-%]",
     require     => Rails::Seed::Db["seed_aeolus_database"]
   }
